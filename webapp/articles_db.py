553b9fed7cef9b4bb97143a6b625bf7b6fca1c6e
kuznetsovroman41 <romank04052006@gmail.com>
2025-06-24 14:35:29 +0600

    создал модель task и зарегистрировал в админке

--- webapp/articles_db.py
+++
@@ -1,9 +0,0 @@
-class ArticlesDB:
-    articles = []
-    cat_avatar = "images/smile.png"
-
-
-    @staticmethod
-    def set_new_avatar():
-        ArticlesDB.cat_avatar = "images/cat.jpg"
-
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    description = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, default='new')
+    due_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.description

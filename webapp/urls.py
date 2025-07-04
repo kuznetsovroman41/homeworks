d2fb25e264eae85fc3f3384f7cdae6fadd81d376
kuznetsovroman41 <romank04052006@gmail.com>
2025-07-04 14:31:51 +0600

    обновил пути

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import task_list, add_task, delete_task, task_detail
+from .views import task_list, add_task, edit_task, delete_task, task_detail
 
 urlpatterns = [
     path('', task_list, name='task_list'),
     path('add/', add_task, name='add_task'),
-    path('delete/<int:task_id>/', delete_task, name='delete_task'),
-    path('task/<int:task_id>/', task_detail, name='task_detail')
+    path('<int:pk>/delete/', delete_task, name='delete_task'),
+    path('<int:pk>/edit/', edit_task, name='edit_task'),
+    path('<int:pk>/', task_detail, name='task_detail'),
+
 ]
+
 
